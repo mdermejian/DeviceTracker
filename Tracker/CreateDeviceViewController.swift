15ec05161998e9ccde8c88dc8ed066f81404ed2b
Marc Dermejian <marcdermejian@yahoo.fr>
2016-12-20 22:14:55 +0000

    - added CreateDeviceView Controller - added DeviceDetailViewController

---
+++ Tracker/CreateDeviceViewController.swift
@@ -0,0 +1,144 @@
+//
+//  CreateDeviceViewController.swift
+//  Tracker
+//
+//  Created by Marc Dermejian on 17/12/2016.
+//  Copyright © 2016 Marc Dermejian. All rights reserved.
+//
+
+import UIKit
+
+class CreateDeviceViewController: UIViewController, UITextFieldDelegate {
+
+	
+	// MARK: - stored properties
+	var device: Device?
+	
+	// MARK: - computed properties
+	var isSaveEnabled: Bool {
+		return deviceTextField.text != ""
+			&& osTextField.text != ""
+			&& manufacturerTextField.text != ""
+	}
+	
+	var textFieldsNotEmpty: Bool {
+		return deviceTextField.text != ""
+			|| osTextField.text != ""
+			|| manufacturerTextField.text != ""
+	}
+	
+	private var deviceParams: [String : AnyObject] {
+		let params = [DeviceKey.Device : deviceTextField.text,
+		        DeviceKey.OS : osTextField.text,
+		        DeviceKey.Manufacturer : manufacturerTextField.text
+		]
+		
+		return params as [String : AnyObject]
+	}
+	
+	private func deviceFromUserInput () -> Device {
+		let device = Device(device: deviceTextField.text!,
+		              os: osTextField.text!,
+		              manufacturer: manufacturerTextField.text!)
+		device.syncStatus = .created
+		return device
+	}
+	
+	// MARK: - IBOutlets
+	@IBOutlet weak var deviceTextField: UITextField! {
+		didSet {
+			NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: deviceTextField, queue: OperationQueue.main) { (notification) in
+				self.updateSaveButtonState ()
+			}
+		}
+	}
+	
+	@IBOutlet weak var osTextField: UITextField! {
+		didSet {
+			NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: osTextField, queue: OperationQueue.main) { (notification) in
+				self.updateSaveButtonState ()
+			}
+		}
+	}
+	
+	@IBOutlet weak var manufacturerTextField: UITextField! {
+		didSet {
+			NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: manufacturerTextField, queue: OperationQueue.main) { (notification) in
+				self.updateSaveButtonState ()
+			}
+		}
+	}
+	
+	@IBOutlet weak var saveButton: UIBarButtonItem! {
+		didSet {
+			saveButton.isEnabled = false
+		}
+	}
+	
+	// MARK: - IBActions
+	@IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+		if textFieldsNotEmpty {
+			presentActionSheet()
+		}else {
+			dismiss()
+		}
+	}
+	
+	@IBAction func saveTapped(_ sender: UIBarButtonItem) {
+		dismissKeyboard()
+		self.device = deviceFromUserInput()
+		self.performSegue(withIdentifier: "unwindToDevicesTableView", sender: self)
+	}
+	
+	@IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+		dismissKeyboard()
+	}
+
+	// MARK: UIViewController lifecycle
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+		deviceTextField.becomeFirstResponder()
+	}
+	
+	// MARK: - Helper funcs
+	private func updateSaveButtonState () {
+		saveButton.isEnabled = isSaveEnabled
+	}
+
+	private func dismiss () {
+		presentingViewController?.dismiss(animated: true)
+	}
+
+	private func dismissKeyboard () {
+		deviceTextField.resignFirstResponder()
+		osTextField.resignFirstResponder()
+		manufacturerTextField.resignFirstResponder()
+	}
+
+	private func presentActionSheet () {
+		let alertController = UIAlertController(title: nil, message: "You have entered some information. By continuing you will lose all entered data", preferredStyle: .actionSheet)
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+		alertController.addAction(cancelAction)
+		
+		let destroyAction = UIAlertAction(title: "Dismiss", style: .destructive) { (action) in
+			self.dismiss()
+		}
+		alertController.addAction(destroyAction)
+		
+		self.present(alertController, animated: true)
+	}
+	
+	// MARK: - UITextField delegate
+	func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+		
+		switch textField {
+		case deviceTextField: osTextField.becomeFirstResponder()
+		case osTextField: manufacturerTextField.becomeFirstResponder()
+		case manufacturerTextField: manufacturerTextField.resignFirstResponder()
+		default: break
+		}
+		
+		return true
+	}
+}
---
+++ Tracker/DeviceDetailViewController.swift
@@ -0,0 +1,111 @@
+//
+//  DeviceDetailViewController.swift
+//  Tracker
+//
+//  Created by Marc Dermejian on 17/12/2016.
+//  Copyright © 2016 Marc Dermejian. All rights reserved.
+//
+
+import UIKit
+
+class DeviceDetailViewController: UIViewController {
+
+	// MARK: - stored properties
+	var device: Device!
+	
+	// MARK: - IBOutlets
+	@IBOutlet weak var deviceLabel: UILabel!
+	@IBOutlet weak var osLabel: UILabel!
+	@IBOutlet weak var manufacturerLabel: UILabel!
+	@IBOutlet weak var checkedOutLabel: UILabel!
+	@IBOutlet weak var checkInOutButton: UIButton!
+	
+	
+	// MARK: UIViewController lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		title = "Detail"
+		updateUI()
+    }
+	
+	// MARK: UI Update
+	private func updateUI () {
+		if let device = device {
+			deviceLabel.text = device.model
+			osLabel.text = device.os
+			manufacturerLabel.text = device.manufacturer
+			checkedOutLabel.text = device.status
+			checkInOutButton.isHidden = false
+
+			if device.isCheckedOut != nil {
+				let title = device.isCheckedOut! ? "Check In" : "Check Out"
+				checkInOutButton.setTitle(title, for: .normal)
+			}
+		}
+	}
+
+	// MARK: - IBActions
+	@IBAction func checkInOut(_ sender: UIButton) {
+		
+		if let isCheckedOut = device.isCheckedOut{
+			isCheckedOut ? checkIn() : showAlert()
+		}
+		
+	}
+	
+	// MARK: - Helper funcs
+	private func checkIn() {
+
+		device.isCheckedOut = false
+		
+		// Only set syncStatus to .updated if the deviceID is not nil
+		// a nil deviceID means the object was created locally and has not been synced yet
+		// In this case, do not set status to updated
+		device.syncStatus = device.id != nil ? .updated : .created
+
+		
+		updateUI()
+	}
+
+	private func checkOut(usingName name: String) {
+
+		device.isCheckedOut = true
+		device.lastCheckedOutBy = name
+		device.lastCheckedOutDate = Date()
+		
+		// Only set syncStatus to .updated if the deviceID is not nil
+		// a nil deviceID means the object was created locally and has not been synced yet
+		// In this case, do not set status to updated
+		device.syncStatus = device.id != nil ? .updated : .created
+		
+		
+		updateUI()
+	}
+	
+	private func showAlert () {
+		let alertController = UIAlertController(title: "Last Checked out by", message: nil, preferredStyle: .alert)
+		
+		let checkoutAction = UIAlertAction(title: "Check Out", style: .default) { (_) in
+			let nameTextField = alertController.textFields![0] as UITextField
+			
+			//call checkOut function
+			self.checkOut(usingName: nameTextField.text!)
+		}
+		checkoutAction.isEnabled = false
+		
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+		
+		alertController.addTextField { (textField) in
+			textField.placeholder = "Enter name"
+			
+			NotificationCenter.default.addObserver(forName: NSNotification.Name.UITextFieldTextDidChange, object: textField, queue: OperationQueue.main) { (notification) in
+				checkoutAction.isEnabled = textField.text != ""
+			}
+		}
+		
+		alertController.addAction(checkoutAction)
+		alertController.addAction(cancelAction)
+		
+		present(alertController, animated: true)
+	}
+}
